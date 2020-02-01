06098f7456d111cbd13e712032fc27e60a554d87
Joe Bailey <baileyrva@gmail.com>
2020-02-01 09:14:37 -0500

    tweaked server setup and descriptions

--- server.js
+++ server.js
@@ -1,74 +1,87 @@
-let express = require("express"); 
-let path = require("path"); 
-let db = require("./db/db.json"); 
-let fs = require("fs"); 
-let util =- require("util"); 
+let express = require("express");
+let path = require("path");
+let db = require("./db/db.json");
+let fs = require("fs");
+let util = require("util");
 
-let app = express(); 
+let app = express();
 
 const writeFileAsync = util.promisify(fs.writeFile);
-const readFileAsync = util.promisify(fs.readFile); 
+const readFileAsync = util.promisify(fs.readFile);
 
+const PORT = 3000;
 
-app.use(express.urlencoded({
+//Express middleware
+app.use(
+  express.urlencoded({
     extended: true
-}));
+  })
+);
+app.use(express.json());
 
-app.use(express.json()); 
+//static files given from homework 
+app.use(express.static(path.join(__dirname, "public")));
 
 
-//static webpages that were given at start of homework
-app.use(express.static(path.join(__dirname, 'public'))); 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+//create routing to serve notes html when the user clicks the button
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-//beginning of API usage GET/POST/DELETE 
-
-app.get("api/notes", function (req, res) {
-    readFileAysync("./db/db.json", "utf8").then(function (data) {
-        data = JSON.parse(data)
-        return res.json(data); 
-    });  
+//reading the db.json file 
+app.get("/api/notes", function(req, res) {
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
+    data = JSON.parse(data);
+    // console.log(data)
+    return res.json(data);
+  });
 });
 
-//posting to the array while appending latest note 
-app.post("api/notes", function (req, res) {
-    let newNotes = req.body; 
-    readFileAysync("./db/db.json", "utf8").then(function (data) {
-        data = JSON.parse(data)
-        data.push(newNotes); 
-        data[data.length - 1].id=data.length-1; 
-        writeFileAsync("./db/db.json", JSON.stringify(data)); 
-    });
-    res.send("Created some notes!") 
+//recieves a note and then adds it to the list on left hand side 
+app.post("/api/notes", function(req, res) {
+  var newNotes = req.body;
+
+  //read the db.json file to grab the arrays of object and return json
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
+    data = JSON.parse(data);
+    data.push(newNotes);
+
+    // appending the new note
+    data[data.length - 1].id = data.length - 1;
+
+    //updating database file 
+    writeFileAsync("./db/db.json", JSON.stringify(data));
+  });
+  res.send("created notes!");
 });
 
+app.delete("/api/notes/:id", function(req, res) {
+  var idReceived = req.params.id;
+  console.log(idReceived);
 
-//deleting newest entry
-app.delete("/api/notes/:id", function (req, res) {
-    var idInput = req.params.id
+  //read the db.json file to grab the arrays of object and return json
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
+    //turn object into string
+    data = JSON.parse(data);
 
-    readFileAysync("./db/db.json", "utf8").then(function (data) {
-        data = JSON.parse(data)
+    //array splice will remove the position I ask it
+    data.splice(idReceived, 1);
 
-        data.splice(idInput, 1); 
+    //the for loop to reset the index of the array
+    for (let i = 0; i < data.length; i++) {
+      data[i].id = i;
+    }
 
-        for (let i = 0; i < data.length; i++) {
-            data[i].id=1;
-        };
-
-        writeFileAsync("./db/db.json", JSON.stringify(data)); 
-    });
-    res.send("Deleted!")
+    //once the new note is added to the array from db.json file then write the upated changes
+    writeFileAsync("./db/db.json", JSON.stringify(data));
+  });
+  res.send("cleared!");
 });
 
-//port identification for local server 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(PORT, function() {
+  console.log("listening on port " + PORT);
 });
